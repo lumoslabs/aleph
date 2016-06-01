2fd2e5b37c42ea8f613f0eb7c502c0e38b2a3938
dollschasingmen <andrew@lumoslabs.com>
2016-05-31 18:14:58 -0700

    expose foreman workers

--- bin/executables/run.rb
+++ bin/executables/run.rb
@@ -1,14 +1,7 @@
 module AlephExecutables
-  class Run
-    def initialize(options)
-      @rails_env = options[:rails_env] || 'development'
-      @dotenv = options[:dotenv]
-    end
-
+  class Run < CapitalistPig
     def execute!
-      cmd = "RAILS_ENV=#{@rails_env} bundle exec foreman start"
-      cmd += "--env #{@dotenv}" if @dotenv
-      system cmd
+      oppress_all!
     end
   end
 end
