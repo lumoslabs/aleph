b83d37530c2bac047819d640794561360e5c0de3
frankhsu <frank.j.hsu@gmail.com>
2016-12-06 17:17:47 -0800

    make s3_region configurable

--- lib/aws_s3.rb
+++ lib/aws_s3.rb
@@ -1,7 +1,9 @@
 module AwsS3
   class << self
+    S3_REGION = APP_CONFIG['s3_region'] || 'us-east-1'
+
     def resource
-      @s3 ||= Aws::S3::Resource.new(region: 'us-east-1')
+      @s3 ||= Aws::S3::Resource.new(region: S3_REGION)
     end
   end
 end
