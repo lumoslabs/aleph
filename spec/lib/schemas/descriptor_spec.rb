34f6930e0e54027d066dc8afe66be5216916b8bc
PolRod <pol@lumoslabs.com>
2017-04-11 16:25:18 -0700

    Spec for schema_refresh

---
+++ spec/lib/schemas/descriptor_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Schemas::Descriptor do
+  let(:query) do
+    [
+      {"table_schema"=>"incarnadine", "table_name"=>"drop_leaf", "column_name"=>"doric", "udt_name"=>"int8", "character_maximum_length"=>nil},
+      {"table_schema"=>"gingerline", "table_name"=>"gateleg", "column_name"=>"ionic", "udt_name"=>"int8", "character_maximum_length"=>nil},
+      {"table_schema"=>"pervenche", "table_name"=>"trestle_1", "column_name"=>"corinthian", "udt_name"=>"int4", "character_maximum_length"=>nil},
+      {"table_schema"=>"pervenche", "table_name"=>"trestle_2", "column_name"=>"corinthian", "udt_name"=>"int4", "character_maximum_length"=>nil},
+      {"table_schema"=>"verditer", "table_name"=>"refectory_1", "column_name"=>"tuscan", "udt_name"=>"varchar", "character_maximum_length"=>"36"},
+      {"table_schema"=>"verditer", "table_name"=>"refectory_2", "column_name"=>"tuscan", "udt_name"=>"varchar", "character_maximum_length"=>"36"},
+      {"table_schema"=>"verditer", "table_name"=>"poker", "column_name"=>"tuscan", "udt_name"=>"varchar", "character_maximum_length"=>"36"}
+    ]
+  end
+  let(:filtered_query) do
+    [
+      {"table_schema"=>"incarnadine", "table_name"=>"drop_leaf", "column_name"=>"doric", "udt_name"=>"int8", "character_maximum_length"=>nil},
+      {"table_schema"=>"gingerline", "table_name"=>"gateleg", "column_name"=>"ionic", "udt_name"=>"int8", "character_maximum_length"=>nil},
+      {"table_schema"=>"pervenche", "table_name"=>"trestle_1", "column_name"=>"corinthian", "udt_name"=>"int4", "character_maximum_length"=>nil}
+    ]
+  end
+
+  subject { described_class.new('admin') }
+
+  describe '#table_columns' do
+    context 'with no blacklisted items' do
+      before do
+        allow(YAML).to receive(:load_file).and_return({ "verditer"=>['poker', 'ref*'], "pervenche"=>['potato', 'trestle_2'] })
+        allow_any_instance_of(described_class).to receive(:exec_schema_query).and_return(query)
+      end
+      it 'returns all tables' do
+        expect(subject.instance_eval { refresh_schema }).to eq(filtered_query)
+      end
+    end
+  end
+end
