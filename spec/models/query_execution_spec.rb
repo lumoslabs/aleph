f03098b8462371ca89e50402877b1420e0855738
PolRod <pol@lumoslabs.com>
2017-06-15 14:30:49 -0700

    Expect to be marked as failed

--- spec/models/query_execution_spec.rb
+++ spec/models/query_execution_spec.rb
@@ -6,14 +6,15 @@ describe QueryExecution do
 
     context 'when it takes longer than we want to wait for' do
       before do
+        stub_const("QueryExecution::QUERY_TIMEOUT", 1) # 1(one) second
         stub_github_calls
         allow(Role).to receive(:configured_connections) { ['admin'] }
         allow(RedshiftConnectionPool).to receive_message_chain('instance.get') { sleep(2) }
-        APP_CONFIG['query_timeout'] = 1 # 1(one) second
       end
 
-      it 'times out if it takes too long' do
-        expect(QueryExecution.perform(result.id, 'admin')).to raise_error(Timeout::Error)
+      it 'marks the result as failed' do
+        expect_any_instance_of(Result).to receive(:mark_failed!)
+        QueryExecution.perform(result.id, 'admin')
       end
     end
   end
