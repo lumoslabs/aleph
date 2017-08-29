32d79e63c2864e542ec1e6babdd68f49f0f30873
dollschasingmen <andrew@lumoslabs.com>
2017-08-29 15:34:47 -0700

    add some spaces for style

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,9 @@
 module Admin
   class BaseController < ApplicationController
     before_filter :verify_admin
+
     private
+
     def verify_admin
       Rails.logger.info("am i an admin??? #{current_user.try(:admin?)}")
       redirect_to root_url unless current_user.try(:admin?)
