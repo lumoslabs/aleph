3226893e1dc5993409220e786df8c031e15047e7
dollschasingmen <andrew@lumoslabs.com>
2017-08-29 15:35:01 -0700

    remove print line

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,7 +5,6 @@ module Admin
     private
 
     def verify_admin
-      Rails.logger.info("am i an admin??? #{current_user.try(:admin?)}")
       redirect_to root_url unless current_user.try(:admin?)
     end
   end
